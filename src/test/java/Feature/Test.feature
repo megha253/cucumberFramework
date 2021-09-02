a0f702d9c09b7a0fac6c11389163725532ae315f
megha253 <megha253goyal@gmail.com>
2021-09-02 14:07:09 +0530

    modified and delets files1 (#2)

--- src/test/java/Feature/Test.feature
+++ src/test/java/Feature/Test.feature
@@ -1,7 +1,7 @@
-
 Feature: LoginPage
 
   Scenario: To log into freeCRM application
-    Given Navigate on the page
-    And enter the username in the field
-
+    Given Open the url to navigate on freeCRM application
+    And enter the username and password in the field
+    When click on submit button
+    Then check user is navigated on home page
--- target/Destination/report.js
+++ target/Destination/report.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Test1.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Test.feature");
 formatter.feature({
-  "line": 2,
+  "line": 1,
   "name": "LoginPage",
   "description": "",
   "id": "loginpage",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "line": 4,
+  "line": 3,
   "name": "To log into freeCRM application",
   "description": "",
   "id": "loginpage;to-log-into-freecrm-application",
@@ -15,27 +15,51 @@ formatter.scenario({
   "keyword": "Scenario"
 });
 formatter.step({
-  "line": 5,
-  "name": "Navigate on the page",
+  "line": 4,
+  "name": "Open the url to navigate on freeCRM application",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 6,
-  "name": "enter the username in the field",
+  "line": 5,
+  "name": "enter the username and password in the field",
   "keyword": "And "
 });
+formatter.step({
+  "line": 6,
+  "name": "click on submit button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "check user is navigated on home page",
+  "keyword": "Then "
+});
 formatter.match({
-  "location": "Test1stepdef.navigate()"
+  "location": "Test.navigate_on_the_page()"
 });
 formatter.result({
-  "duration": 19934877700,
+  "duration": 17058275000,
   "status": "passed"
 });
 formatter.match({
-  "location": "Test1stepdef.username()"
+  "location": "Test.enter_the_username_in_the_field()"
 });
 formatter.result({
-  "duration": 17159106000,
+  "duration": 536411800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Test.click_on_submit_button()"
+});
+formatter.result({
+  "duration": 7637396400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Test.check_user_is_navigated_on_home_page()"
+});
+formatter.result({
+  "duration": 114678200,
   "status": "passed"
 });
 });
